6eaa358905b37c25460477a0a89935f9703b7c04
Samson Akanbi <samson.akanbi@outlook.com>
2022-12-25 17:34:14 +0100

    Provided default values to the search box.

--- app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
@@ -1,5 +1,8 @@
 package com.codinginflow.imagesearchapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.codinginflow.imagesearchapp.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -7,4 +10,13 @@ import javax.inject.Singleton
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
 
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
 }
